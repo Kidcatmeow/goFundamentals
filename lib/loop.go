a69c41a0e9dab1484152d1f9375ef7ede548b20b
Kidcatmew <kidcatmew@gmail.com>
2024-01-25 10:23:11 +0700

    loop complete

--- lib/loop.go
+++ lib/loop.go
@@ -22,16 +22,52 @@ func main() {
 
 	//do-while loop
 	//loop body execute atleast once
-	b := 0
-	for b := true; b; b < 10 {
+	b := 10
+	for next := true; next; next = b < 5 {
 		fmt.Println(b)
 		b++
 	}
 
-	// //forever loop
+	d := 0
+	for next := true; next; next = d < 5 {
+		fmt.Println(d)
+		d++
+	}
+
+	//forever loop
 	c := 0
 	for {
 		fmt.Println()
 		c++
 	}
+
+	//Print from slices or array
+	slice := []string{"Kidcat", "Mochi", "Honey", "Lemon"}
+	// array := [4]string{"Kidcat", "Mochi", "Honey", "Lemon"}
+	fmt.Println("\n for basic")
+
+	for i := 0; i < len(slice); i++ {
+		value := slice[i]
+		fmt.Println(i, ":", value)
+	}
+
+	//range return two values = index and value
+	fmt.Printf("\n for range slice")
+	for index, value := range slice {
+		fmt.Println(index, ":", value)
+	}
+
+	//but if we don't want to use index with range then put _ instead
+	//range return two values = index and value
+	fmt.Println("\nOnly value")
+	for _, value := range slice {
+		fmt.Println("Only value:", value)
+	}
+
+	//if we want only index
+	fmt.Println("\nOnly index")
+	for index := range slice {
+		fmt.Println("Only index", index)
+	}
+
 }
